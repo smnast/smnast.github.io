a9018881fc7df1c437f15a3ce14905604d7b2bb5
Simon Ashton <simonashton.dev@gmail.com>
2024-09-04 10:48:38 -0700

    Improve project descriptions

--- src/components/Contents/Sections/ProjectsSection.tsx
+++ src/components/Contents/Sections/ProjectsSection.tsx
@@ -1,9 +1,17 @@
 import HueMasterResult from '../../../assets/images/projects/huemaster_result.png'
 import Robot34300A from '../../../assets/images/projects/34300A.jpg'
-import Robotica from '../../../assets/images/projects/robotica.png'
+import RoboticaSnap from '../../../assets/images/projects/robotica_snap.png'
+import RoboticaGroup from '../../../assets/images/projects/robotica_group.png'
 import { CodeBlock } from 'react-code-blocks'
 
 const ProjectsSection = () => {
+    const equalsSynthScriptSample = `a <- 2
+output(a = 2) # true`;
+
+    const loopSynthScriptSample = `for i in 1..5 {
+    output(i)
+}`;
+
     const synthScriptSample = `function count_words_in_file(file_path) {
     file_text <- read(file_path)
     current_word <- ""
@@ -27,6 +35,9 @@ const ProjectsSection = () => {
 
 output(count_words_in_file("example_file.txt"))`;
 
+    const hueMasterSample = `cursor: capitaine-cursors-$$LIGHT?light:dark$$
+background: $$BACKGROUND.alpha(-20)$$`;
+
     return (
         <>
             <b>I have worked on countless projects throughout my life. Here are some of my favourites.
@@ -38,106 +49,122 @@ output(count_words_in_file("example_file.txt"))`;
                     and accessible built-in functions, including for reading and writing to files.
                 </p>
                 <p>
-                    I researched every step in crafting a programming language from scratch. I analyzed other languages,
-                    like C++ and even Raku (Perl 6), determining what makes a good language, and why others fail to gain traction.
+                    The language is written in C++, without the use of extenral libraries like Yacc or Bison. It uses a polymorphism approach
+                    to dynamic typing, along with an <code>std::map</code> for the symbol table and storing variables/functions. The abstract syntax tree
+                    is built recursively, with polymorphism used for defining classes for the tree nodes.
                 </p>
                 <p>
-                    The language is written in C++, without the use of libraries like Yacc or Bison. It uses polymorphism for the dynamic typing,
-                    along with design patterns including <a href="https://refactoring.guru/design-patterns/visitor">Visitor</a>,
-                    and <a href="https://refactoring.guru/design-patterns/composite">Composite</a> for
-                    the <a href="https://en.wikipedia.org/wiki/Abstract_syntax_tree">Abstract Syntax Tree</a>.
+                    Many design decisions were made to create a more intuitive language. For instance, the assignment operator is set to an arrow, rather than an equals sign.
+                    Thus, the comparison operator can be a singular equals sign, instead of two:
                 </p>
+                <CodeBlock text={equalsSynthScriptSample} />
+                <p>
+                    No more complex for-loop syntax; SynthScript chooses a Python approach, with a more intuitive <code>range</code> operator:
+                </p>
+                <CodeBlock text={loopSynthScriptSample} />
                 <p>
                     Here is an example of a program written in SynthScript, to count the number of words in a file:
                 </p>
                 <CodeBlock text={synthScriptSample} />
                 <p>
-                    This was, however, before I discovered testing, so I am not certain that the language is "safe" to use.
+                    One major improvement to consider for this language is adding a testing framework, like doctest. This would help to ensure
+                    that the language functions correctly and can be used for larger projects. For instance, tests with small features of SynthScript, like a function returning
+                    a value, could be added to verify that the program works correctly.
                 </p>
             </div>
             <a href="https://github.com/smnast/HueMaster/"><h1>HueMaster</h1></a>
             <div>
                 <p>
-                    HueMaster was one of my favourite types of projects: one where I find a gap in my life and
-                    fill it with software. I was setting up my laptop with an aesthetic colour scheme and
-                    quickly realized that I had absolutely no idea how to create one. Instead, I chose to create
-                    Huemaster, which determines a pleasing colour scheme based on my wallpaper.
+                    HueMaster is a colour scheme generator based on the idea of extracting a useable colour scheme from an image (a wallpaper, for instance).
+                    It is written in C++, using the OpenCV and toml11 libraries.
                 </p>
                 <p>
-                    I used OpenCV and a <i>lot</i> of heuristics to compute a set of colours based on the given wallpaper,
-                    making sure to separate the code into concise classes using the Single Responsibility Principle.
+                    It uses OpenCV's K-Means clustering (<code>cv::kmeans</code>) and several heuristics to compute a set of colours based on the given wallpaper.
+                    The heuristics involve calculating a score for a given colour based on factors including distance to other colours in the colour scheme, contrast with the
+                    background colour and proportion of the colour in the original image. The colour with the best score is chosen.
+                </p>
+                <p>
+                    I made sure to separate the code into concise classes using the Single Responsibility Principle.
                     For instance, the part of the program that actually writes the config files is distributed
                     into <code>configurator.cpp</code>, <code>parser.cpp</code>, and <code>writer.cpp</code> for
                     reading the HueMaster config, reading the individual programs's config files and writing the final
                     config files, respectively.
                 </p>
                 <p>
+                    HueMaster also includes a domain specific language for specifying colours in config files. The language is formed by a chain of commands,
+                    including <code>lighten</code>, and <code>alpha</code>. It also includes a ternary operator in the form <code>LIGHT?STRING_1:STRING_2</code> to
+                    select a string based on if the colour scheme will be light or dark.
+                </p>
+                <CodeBlock text={hueMasterSample} />
+                <p>
                     This is my <a href="https://github.com/smnast/void-nullptr">current i3 setup</a> as of writing this, using HueMaster for the colour scheme:
                 </p>
                 <img src={HueMasterResult} alt="Linux i3 setup using HueMaster" />
+                <p>
+                    I plan to incorporate machine learning into the project in the future (in place of the current heuristics) to assist in producing more reliable, aesthetic colour schemes.
+                </p>
             </div>
             <a href="https://github.com/smnast/Robot10/"><h1>Robot10</h1></a>
             <div>
                 <p>
-                    A group of my high school friends and I decided to create a VEX robotics superteam
-                    out of essentially the greatest members of the club at the time. My role on our
-                    team was to program the robot, both for driver control and the 15-second autonomous period
-                    at the beginning of every match.
+                    Robot10 is the final version of my 2023-2024 VEX Robotics robot code. It is written in C++, using the third-party library <a href="https://github.com/LemLib/LemLib">LemLib</a>.
+                    LemLib abstracts a lot of the complex features of creating autonomous programs, including <a href="https://en.wikipedia.org/wiki/Proportional%E2%80%93integral%E2%80%93derivative_controller">PID</a> control
+                    and <a href="https://wiki.purduesigbots.com/software/odometry">odometry</a>.
+                </p>
+                <p>
+                    The robot's path was found using a combination of online map programs, field measurements and trial-and-error. Other than driving, the robot needed to control other functions both autonomously and
+                    with driver input. These included: "wings" (essentially snow plows, and were a vital part of that season's meta) and a catapult that doubled as a climbing mechanism.
                 </p>
                 <img src={Robot34300A} alt="34300A VEX robot" />
                 <p>
-                    I was able to create a smooth driver control and complex autonomous program using the third-party library, <a href="https://github.com/LemLib/LemLib">LemLib</a>.
-                    The code is written in C++, and could have used more separation and documentation to improve the readability.
-                </p>
-                <p>
-                    While the autonomous was "accurate", it certainly was not "reliable". Many times during our last competition, the robot would turn around and
-                    try to escape the field! It turned out that the upload was corrupted, and reuploading it fixed the problem, but my team never trusted my autonomous
-                    after that. This taught me the importance of <a href="https://en.wikipedia.org/wiki/2024_CrowdStrike_incident">testing the software before going to the competition</a>, even
-                    if everything seems like it should work fine.
+                    There were many times during our last competition where the robot would turn around and try to escape the field.
+                    It turned out that the upload was corrupted, and reuploading it fixed the problem.
+                    This taught me the importance of testing the software before the matches began, even if everything <i>seems</i> like it should work.
                 </p>
             </div>
             <a href="https://github.com/smnast/Robotica"><h1>Robotica</h1></a>
             <div>
                 <p>
-                    Ah, Robotica. One of the most complex projects I have ever created. And yet, my least favourite.
-                    I will never touch that project ever again. Robotica is written in C# using the <a href="https://godotengine.org">Godot game engine</a>,
-                    with all the parts being modified and textured in Blender.
-                </p>
-                <img src={Robotica} alt="Interface of Robotica" />
-                <p>
-                    It started off as a clone of <a href="https://protobot.web.app/">Protobot</a>, a Unity application
-                    designed for rapid prototyping of VEX robots. Really, that's all it is: just a clone of Protobot.
+                    Robotica is a VEX prototyping/CAD software designed using the <a href="https://godotengine.org">Godot</a> game engine with C#, and Blender for the 3D models.
+                    It was designed as an improvement of another VEX prototyping software, <a href="https://protobot.web.app/">Protobot</a>, with plans to add many new features
+                    not found in its predecessor.
                 </p>
                 <p>
-                    The reason it exists was because my classmates and I (even my robotics teacher) had numerous comments
-                    about how Protobot should be different, including bugfixes or new features. Robotica had the potential
-                    to be a solution to this problem.
+                    This was the first time that I worked on a project for more than a couple months. In total, I spent three months working on importing new parts and
+                    adding core functionality. However, the project was rushed, creating many problems for me down the line.
                 </p>
                 <p>
-                    Unfortunately, the project was "rushed". The code is poorly documented
-                    and smells of every possible code smell. I wrote this project before I learned basic design patterns, so it is poorly
-                    thought-out in general. Instead of being motivated to write clean, concise code, I used the number of lines of code as an
-                    indicator of my progress, so the <code>Control.cs</code> file totals 1490 lines of code (including 2 lines of comments).
+                    Some key features of Robotica include a transform tool for moving parts around, along with a move tool which allows the user to quickly snap pieces together:
+                </p>
+                <img src={RoboticaSnap} alt="A nut being snapped onto a screw in Robotica" />
+                <p>
+                    as well as automatic grouping of parts, built upon a system of "fasteners", "inserts" and "holes":
                 </p>
                 <p>
-                    I enjoy thinking back to this project and how I could have written it differently. I have thought about rewriting the program
-                    from scratch before, but I have more important things to do now.
+                    The main improvements of Robotica over Protobot include: significantly better performance (often 10x the framerate), faster load times, more parts,
+                    more consistent automatic grouping, manual grouping, consistent saving/loading functionality and a <i>lot</i> more settings.
+                </p>
+                <img src={RoboticaGroup} alt="A C-channel automatically grouped with connected parts." />
+                <p>
+                    Several improvements could be made to the code design. For instance, separating the long <code>Control.cs</code> class would help keep the control
+                    logic cleaner and maintainable. The part system could also be more abstracted to allow for easily adding new parts, without the need to edit
+                    multiple files to introduce one part. Without these improvements, the project is difficult to maintain and continue.
                 </p>
             </div>
             <a href="https://github.com/smnast/cp-tool"><h1>cp-tool</h1></a>
             <div>
                 <p>
-                    Like HueMaster, cp-tool exists to solve a problem that I experienced in my daily life. I was setting up an environment
-                    to practice competitive programming in NeoVim, but lacked a simple interface to create a folder with a
-                    single <code>.cpp</code> file for my solution.
+                    cp-tool is a simple Python script designed to create folders for competitive programming solution files. It supports creating files for an invidual problem,
+                    or for an entire contest. Since many competitive programmers prefer to start their solution with a "template", cp-tool allows users to specify a template
+                    and copies the desired code to their solution file.
                 </p>
                 <p>
-                    To solve the problem, I wrote a simple Python script with shallow functions each designed to do one task. To shorten the command to
-                    just <code>cpt</code> instead of <code>python cp-tool.py</code>, I wrote a separate shell script, with the added benefit of
+                    To shorten the command to just <code>cpt</code> instead of <code>python cp-tool.py</code>, I wrote a separate shell script, with the added benefit of
                     being able to automatically <code>cd</code> into the new directory by sourcing the script (or <code>alias cpt='source cpt'</code>).
                 </p>
                 <p>
-                    (I have since learned how to install a program using Make, while this project required a sequence of clones and symlinks)
+                    One improvement to this program would be a proper means of installing it (CMake or similar), rather than instructing prospective users to symlink the files to
+                    their <code>bin</code> directory.
                 </p>
             </div>
         </>
