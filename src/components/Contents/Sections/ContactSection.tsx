8648f52d9516f002e2f71c182c21a06bfae6f7e2
Simon Ashton <simonashton.dev@gmail.com>
2024-09-04 10:49:43 -0700

    Prefer one line for contact information

--- src/components/Contents/Sections/ContactSection.tsx
+++ src/components/Contents/Sections/ContactSection.tsx
@@ -6,36 +6,28 @@ const ContactSection = () => {
             <h1>Contact Information:</h1>
             <div>
                 <p>
-                    <i className="bi bi-envelope"></i> -&nbsp;
-                    <a href="mailto:simonashton.dev@gmail.com">simonashton.dev@gmail.com</a>&nbsp;
-                    (preferred method of communication)
+                    <i className="bi bi-envelope"></i> - <a href="mailto:simonashton.dev@gmail.com">simonashton.dev@gmail.com</a> (preferred method of communication)
                 </p>
                 <p>
-                    <i className="bi bi-telephone"></i> -&nbsp;
-                    <a href="tel:778-917-9843">778-917-9843</a>&nbsp;
+                    <i className="bi bi-telephone"></i> - <a href="tel:778-917-9843">778-917-9843</a>
                 </p>
                 <p>
-                    <i className="bi bi-house-door"></i> -
-                    Please contact me directly if you need my address.
+                    <i className="bi bi-house-door"></i> - Please contact me directly if you need my address.
                 </p>
             </div>
             <h1>More Links:</h1>
             <div>
                 <p>
-                    <i className="bi bi-github"></i> -&nbsp;
-                    <a href="https://github.com/smnast/">smnast</a> (Simon Ashton)
+                    <i className="bi bi-github"></i> - <a href="https://github.com/smnast/">smnast</a> (Simon Ashton)
                 </p>
                 <p>
-                    <i className="bi bi-discord"></i> -
-                    mrpersondev (MrPerson)
+                    <i className="bi bi-discord"></i> - mrpersondev (MrPerson)
                 </p>
                 <p>
-                    <i className="bi bi-youtube"></i> -&nbsp;
-                    <a href="https://youtube.com/@smnast">@smnast</a> (Simon Ashton)
+                    <i className="bi bi-youtube"></i> - <a href="https://youtube.com/@smnast">@smnast</a> (Simon Ashton)
                 </p>
                 <p>
-                    <Icon icon="simple-icons:codeforces" /> -&nbsp;
-                    <a href="https://codeforces.com/profile/MrPerson">MrPerson</a>
+                    <Icon icon="simple-icons:codeforces" /> - <a href="https://codeforces.com/profile/MrPerson">MrPerson</a>
                 </p>
             </div>
         </>
