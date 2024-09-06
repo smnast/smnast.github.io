35c2ed2becb19df47914b8f904f397a21d8847ac
Simon Ashton <simonashton.dev@gmail.com>
2024-09-06 09:49:30 -0700

    Collapsibles and formatting changes

---
+++ src/components/Collapsible.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, useState } from 'react'
+import './Collapsible.css'
+
+interface CollapsibleProps {
+    description: ReactNode,
+    content: ReactNode
+}
+
+const Collapsible = ({ description, content }: CollapsibleProps) => {
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+        <div className="Collapsible">
+            <div className="header">
+                <div className="description">
+                    {description}
+                </div>
+                <div className="icon" onClick={() => setExpanded(!expanded)}>
+                    <i className={'bi bi-chevron-' + (expanded ? 'up' : 'down')}></i>
+                </div>
+            </div>
+            <div className={expanded ? 'expanded' : 'collapsed'}>
+                {content}
+            </div>
+        </div>
+    );
+}
+
+export default Collapsible;
